9f60d839a94a05326916562ab1aa88b9bb3e8418
Simon Emanuel Schmid <simon@edgeandnode.com>
2021-08-06 15:55:15 +0200

    Real world example

--- src/mapping.test.ts
+++ src/mapping.test.ts
@@ -5,27 +5,39 @@ import { ImplementationAdded } from "../generated/CallProxy/CallProxy";
 import { handleImplementationAdded } from "./mapping";
 
 function createImplementationAddedEvent(
-    implementation: string
-    //   initializer: Bytes,
-    //   finalize: boolean
+  implementation: string,
+  initializer: string,
+  finalize: boolean
 ): ImplementationAdded {
-    let newImplementationAdded = new ImplementationAdded();
-    newImplementationAdded.parameters = new Array<ethereum.EventParam>();
+  let newImplementationAdded = new ImplementationAdded();
+  newImplementationAdded.parameters = new Array<ethereum.EventParam>();
 
-    let implementationParam = new ethereum.EventParam();
-    implementationParam.value = ethereum.Value.fromAddress(
-        Address.fromString(implementation)
-    );
+  let implementationParam = new ethereum.EventParam();
+  implementationParam.value = ethereum.Value.fromAddress(
+    Address.fromString(implementation)
+  );
 
-    newImplementationAdded.parameters.push(implementationParam);
+  let initializerParam = new ethereum.EventParam();
+  initializerParam.value = ethereum.Value.fromBytes(
+    Bytes.fromHexString(initializer) as Bytes
+  );
 
-    return newImplementationAdded;
+  let finalizeParam = new ethereum.EventParam();
+  finalizeParam.value = ethereum.Value.fromBoolean(finalize);
+
+  newImplementationAdded.parameters.push(implementationParam);
+  newImplementationAdded.parameters.push(initializerParam);
+  newImplementationAdded.parameters.push(finalizeParam);
+
+  return newImplementationAdded;
 }
 
 export function runTests(): void {
-    test("test name", () => {
-        let implementationAddedEvent = createImplementationAddedEvent(
-            "0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7"
-          );
-    })
+  test("test name", () => {
+    let implementationAddedEvent = createImplementationAddedEvent(
+      "0xcc5b2c75cbbd281b2fc4b58c7d5b080d023c92f2",
+      "0x000000000000000000000000894c4a12548fb18eaa48cf34f9cd874fc08b7fc3",
+      false
+    );
+  });
 }
