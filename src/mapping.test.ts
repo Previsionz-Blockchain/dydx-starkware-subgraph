ed7b5d4973f4eadee919298cf517d841bd2ccfbf
Simon Emanuel Schmid <simon@edgeandnode.com>
2021-08-26 19:42:55 +0200

    Add tests for data parsing, but have it commented out for now

--- src/mapping.test.ts
+++ src/mapping.test.ts
@@ -1,8 +1,15 @@
-import { Address, Bytes, ethereum } from "@graphprotocol/graph-ts";
+import {
+  Address,
+  Bytes,
+  ethereum,
+  TypedMap,
+  BigInt,
+} from "@graphprotocol/graph-ts";
 import { clearStore, test, addMetadata } from "matchstick-as/assembly/index";
 // import { log } from "matchstick-as/assembly/log";
 import { ImplementationAdded } from "../generated/CallProxy/CallProxy";
 
+// import { batchOnChainData } from "./batchOnChainData";
 import { handleImplementationAdded } from "./mapping";
 
 function createImplementationAddedEvent(
@@ -34,6 +41,47 @@ function createImplementationAddedEvent(
   return newImplementationAdded;
 }
 
+// /*
+//   Represents a collection of timestamped global funding indices for all assets.
+
+//   :param indices: Map of synthetic asset to its global funding index.
+//   :type indices: Mapping[int, int]
+//   :param timestamp: The timestamp for which the funding indices are valid for.
+//   :type timestamp: int
+//  */
+// class FundingIndicesState {
+//   indices: TypedMap<BigInt, i32>;
+//   timestamp: i32;
+// }
+
+// /*
+//   Holds inforamation about a single position update.
+//   Members:
+//     - position_id: the id of the position the update refers to.
+//     - public_key: the new public key of the position.
+//     - collateral_balance: the new amount of collateral_balance of the position.
+//     - funding_timestamp: The timestamp of the last funding that was applied to the position.
+//     - asset_balance_updates: information regarding the assets that changed in the position
+//     (contains asset_id and balance)
+// */
+// class PositionStateUpdate {
+//   positionId: i32;
+//   publicKey: Address;
+//   collateralBalance: i32;
+//   fundingTimestamp: i32;
+//   assetBalanceUpdates: TypedMap<BigInt, i32>;
+// }
+
+// /*
+// Dataclass for storing the onchain data.
+// */
+// class PerpetualOnChainData {
+//   fundingIndicesMapping: TypedMap<i32, FundingIndicesState>;
+//   updates: PositionStateUpdate[];
+// }
+
+// function parseOnChainData(values: BigInt[]): void {}
+
 export function runTests(): void {
   test("test name", () => {
     let implementationAddedEvent = createImplementationAddedEvent(
@@ -43,4 +91,8 @@ export function runTests(): void {
     );
     handleImplementationAdded(implementationAddedEvent);
   });
+
+  // test("data parsing", () => {
+  //   parseOnChainData(batchOnChainData);
+  // });
 }
